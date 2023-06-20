448864a0217161aab6d7aaf2b092f37eed8daaa4
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-06-20 12:22:00 +0300

    done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,33 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    """Фикстура для создания объекта класса Item."""
+    return Item(name='test_name', price=1.5, quantity=10)
+
+
+def test_item_init(item):
+    """Тест для создания объекта класса Item."""
+    assert item.name == 'test_name'
+    assert item.price == 1.5
+    assert item.quantity == 10
+
+
+def test_item_calculate_total_price(item):
+    """Тест для расчёта стоимости товара."""
+    assert item.calculate_total_price() == 15.0
+
+
+def test_item_apply_discount(item):
+    """Тест для применения скидки к стоимости товара."""
+    item.apply_discount()
+    assert 1.5 * Item.pay_rate == item.price
+    assert item.price == 1.5
+
+
+# def test_item_all(item):
+#     """Тест для проверки всех объектов класса Item."""
+#     assert item.all == [item]
