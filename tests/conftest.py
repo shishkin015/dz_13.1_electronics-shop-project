dd9382d3ca15d2475aebd3c64e173da06213aba4
shishkin015 <shishkin015@gmail.com>
2023-08-27 10:57:40 +0300

    keyboard

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.item import Item
+from src.keyboard import Keyboard
 
 
 @pytest.fixture(scope="session")
@@ -67,5 +68,36 @@ def data_for_test_instantiate_from_csv():
 
     expected = "Смартфон"
     tested = Item.all[0].name
+
+    return tested, expected
+
+
+# Fixtures for testing Keyboard class
+@pytest.fixture(scope="session")
+def data_for_test_str_method_keyboard():
+    instance = Keyboard('Dark Project KD87A', 9600, 5)
+
+    tested = str(instance)
+    expected = "Dark Project KD87A"
+
+    return tested, expected
+
+
+@pytest.fixture(scope="session")
+def data_for_test_language_keyboard():
+    instance = Keyboard('Dark Project KD87A', 9600, 5)
+
+    tested = instance.language
+    expected = "EN"
+
+    return tested, expected
+
+
+@pytest.fixture(scope="session")
+def data_for_test_change_lang_keyboard():
+    instance = Keyboard('Dark Project KD87A', 9600, 5)
+
+    tested = instance.change_lang().language
+    expected = "RU"
 
     return tested, expected
---
+++ tests/test_keyboard.py
@@ -0,0 +1,16 @@
+def test_str_method(data_for_test_str_method_keyboard):
+    tested, expected = data_for_test_str_method_keyboard
+
+    assert tested == expected
+
+
+def test_language(data_for_test_language_keyboard):
+    tested, expected = data_for_test_language_keyboard
+
+    assert tested == expected
+
+
+def test_change_lang(data_for_test_change_lang_keyboard):
+    tested, expected = data_for_test_change_lang_keyboard
+
+    assert tested == expected
